f9fbeefa9e1e4b43a0e15a044255781fed61bf12
Pulkit Choudhary <khauffsaab@gmail.com>
2025-02-01 22:01:20 +0530

    Initial Setup: Created django project and app, added config settings for database connections and restframework

---
+++ bunk_game_strong/bunk_game_strong/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bunk_game_strong project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bunk_game_strong.settings')
+
+application = get_asgi_application()
